bf22dc21267c54396a313630634184ffd423039b
CemS <snmzcm@gmail.com>
2024-12-25 23:17:35 +0300

    Separated fetching modules

--- accesion.py
+++ accesion.py
@@ -3,42 +3,47 @@ from selenium.webdriver.common.by import By
 import time
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import pandas as pd
 
-# Initialize the web driver (make sure the ChromeDriver is in your PATH)
-driver = webdriver.Firefox()  # or webdriver.Firefox() for Firefox
+def accesion():
+    driver = webdriver.Firefox()  # or webdriver.Firefox() for Firefox
 
-try:
-    # Open the search page
-    driver.get('https://www.ncbi.nlm.nih.gov/nuccore')  # Replace with the actual URL of the search page
+    try:
+        # Open the search page
+        driver.get('https://www.ncbi.nlm.nih.gov/nuccore')  # Replace with the actual URL of the search page
 
-    # Locate the input field with id 'term' and enter the search query
-    search_input = driver.find_element(By.ID, 'term')
-    search_input.send_keys('txid545932[Organism:exp]')  # Enter the search term
+        # Locate the input field with id 'term' and enter the search query
+        search_input = driver.find_element(By.ID, 'term')
+        search_input.send_keys('txid545932[Organism:exp]')  # Enter the search term
 
-    # Locate the search button with id 'search' and click it
-    search_button = driver.find_element(By.ID, 'search')
-    search_button.click()  # Click the search button
-    kacadetlistelenecek = WebDriverWait(driver, 30).until( EC.presence_of_element_located((By.XPATH, "//*[text()='20 per page']")) )
-    kacadetlistelenecek.click()
-    menu_item = WebDriverWait(driver, 30).until( EC.presence_of_element_located((By.ID, "ps100")) ) 
-    menu_item.click()
-    time.sleep(8)
-    #Accession Listeleme
-    accessionListele = WebDriverWait(driver, 10).until( EC.presence_of_element_located((By.XPATH, "//*[text()='Summary']")) )
-    accessionListele.click()
-    accession_item = WebDriverWait(driver, 10).until( EC.presence_of_element_located((By.ID, "accnlisttext")) ) 
-    accession_item.click()
-    ######################
-    # Wait for the results to load
-    time.sleep(8)  # Adjust time as necessary or use WebDriverWait for better handling
+        # Locate the search button with id 'search' and click it
+        search_button = driver.find_element(By.ID, 'search')
+        search_button.click()  # Click the search button
+        kacadetlistelenecek = WebDriverWait(driver, 30).until( EC.presence_of_element_located((By.XPATH, "//*[text()='20 per page']")) )
+        kacadetlistelenecek.click()
+        menu_item = WebDriverWait(driver, 30).until( EC.presence_of_element_located((By.ID, "ps100")) ) 
+        menu_item.click()
+        time.sleep(8)
+        #Accession Listeleme
+        accessionListele = WebDriverWait(driver, 10).until( EC.presence_of_element_located((By.XPATH, "//*[text()='Summary']")) )
+        accessionListele.click()
+        accession_item = WebDriverWait(driver, 10).until( EC.presence_of_element_located((By.ID, "accnlisttext")) ) 
+        accession_item.click()
+        ######################
+        # Wait for the results to load
+        time.sleep(8)  # Adjust time as necessary or use WebDriverWait for better handling
 
-    # Optionally, you may want to fetch and print the results here
-    resultText = WebDriverWait(driver, 30).until( EC.presence_of_element_located((By.TAG_NAME, "pre")) )
-    results = resultText.text
-    items = results.split('\n')
-    for item in items:
-        print(item)
+        # Optionally, you may want to fetch and print the results here
+        resultText = WebDriverWait(driver, 30).until( EC.presence_of_element_located((By.TAG_NAME, "pre")) )
+        results = resultText.text
+        items = results.split('\n')
+        for item in items:
+            print(item)
 
-finally:
-    # Close the browser
-    driver.quit()
+
+        return items
+
+    finally:
+        # Close the browser
+        driver.quit()
+
--- fetcher.py
+++ fetcher.py
@@ -4,35 +4,37 @@ import time
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-# Initialize the web driver (make sure the ChromeDriver is in your PATH)
-driver = webdriver.Firefox()  # or webdriver.Firefox() for Firefox
+def fetcher():
+    driver = webdriver.Firefox()
+    try:
+        
+        driver.get('https://www.ncbi.nlm.nih.gov/nuccore')  # Replace with the actual URL of the search page
 
-try:
-    # Open the search page
-    driver.get('https://www.ncbi.nlm.nih.gov/nuccore')  # Replace with the actual URL of the search page
+        # Locate the input field with id 'term' and enter the search query
+        search_input = driver.find_element(By.ID, 'term')
+        search_input.send_keys('txid545932[Organism:exp]')  # Enter the search term
 
-    # Locate the input field with id 'term' and enter the search query
-    search_input = driver.find_element(By.ID, 'term')
-    search_input.send_keys('txid545932[Organism:exp]')  # Enter the search term
+        # Locate the search button with id 'search' and click it
+        search_button = driver.find_element(By.ID, 'search')
+        search_button.click()  # Click the search button
+        kacadetlistelenecek = WebDriverWait(driver, 10).until( EC.presence_of_element_located((By.XPATH, "//*[text()='20 per page']")) )
+        kacadetlistelenecek.click()
+        menu_item = WebDriverWait(driver, 10).until( EC.presence_of_element_located((By.ID, "ps100")) ) 
+        menu_item.click()
 
-    # Locate the search button with id 'search' and click it
-    search_button = driver.find_element(By.ID, 'search')
-    search_button.click()  # Click the search button
-    kacadetlistelenecek = WebDriverWait(driver, 10).until( EC.presence_of_element_located((By.XPATH, "//*[text()='20 per page']")) )
-    kacadetlistelenecek.click()
-    menu_item = WebDriverWait(driver, 10).until( EC.presence_of_element_located((By.ID, "ps100")) ) 
-    menu_item.click()
+        # Wait for the results to load
+        time.sleep(8)  # Adjust time as necessary or use WebDriverWait for better handling
 
-    # Wait for the results to load
-    time.sleep(8)  # Adjust time as necessary or use WebDriverWait for better handling
-
-    # Optionally, you may want to fetch and print the results here
-    results = driver.find_elements(By.CLASS_NAME,'title')  # Updat the selector based on actual results
-    sayac = 1
-    for result in results:
-        print(str(sayac) + ' ' + result.text)
-        sayac= sayac + 1
-
-finally:
+        itemList = []
+        results = driver.find_elements(By.CLASS_NAME,'title')  # Updat the selector based on actual results
+        sayac = 1
+        for result in results:
+            print(str(sayac) + ' ' + result.text)
+            itemList.append(str(sayac) + ' ' + result.text)
+            sayac= sayac + 1
+        return itemList
+    finally:
     # Close the browser
-    driver.quit()
+        driver.quit()
+
+
